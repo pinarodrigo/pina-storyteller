5f6f0511f5b5ab780cc193d690d17386749be85b
Rodrigo Pina Lozano (CI/DAD5.1) <rodrigo.pina@de.bosch.com>
2020-02-23 12:27:53 +0100

    removes test content

--- test/sample.test.js
+++ test/sample.test.js
@@ -6,17 +6,5 @@ for (const p of [new Alexa()]) {
     const testSuite = p.makeTestSuite();
 
     describe(`PLATFORM: ${p.constructor.name} INTENTS`, () => {
-        test('should tell "Hello World!" on LAUNCH', async () => {
-            const conversation = testSuite.conversation();
-
-            const launchRequest = await testSuite.requestBuilder.launch();
-            const responseLaunchRequest = await conversation.send(launchRequest);
-
-            expect(
-                responseLaunchRequest.isTell('Hello World!'),
-            ).toBe(true);
-
-            await conversation.clearDb();
-        });
     });
 }
